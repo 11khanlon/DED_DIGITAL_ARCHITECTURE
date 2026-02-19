430efcee788ebd574ca03a186dd88ac60a7ca34a
Kayleigh Hanlon <11khanlon@gmail.com>
2026-02-19 13:52:37 -0500

    I fixed the laser hierarchy and the machine head

--- RPMI_DATA_DEV/rpmi_ontology.py
+++ RPMI_DATA_DEV/rpmi_ontology.py
@@ -1,3 +1,7 @@
+#%%
+import numpy as np
+import pandas as pd
+import os
 # %%
 #--Create ontology table with parameter names and descriptions--
 
@@ -8,30 +12,91 @@ Realtionships (measured_by_, controlled_by, part_of)
 Queryable structure 
 '''
 
-kinematic_data = pd.DataFrame({
-    "kinematic_id": ["position_id", "velocity_id"],
+#%%
+
+RPMI_machine_data = pd.DataFrame({
+    "parameter_name": [
+        "Box Pressure",
+        "Dust Collector Diff Pressure",
+        "Powder Manifold Pressure Sensor",
+        "Path Setpoint Velocity(inch/min)",
+        "Toolcode Execution Time"
+    ]
 })
 
+''' variables that are helpful for RPMI, but I won't know until I print'''
+   
+#%%
+
+#---machine head data---
+
+machine_head = pd.DataFrame({
+    "machine_head_id": ["MH1"],
+    "description": ["Laser machine head assembly"]
+})
+
+
+machine_head_subsystems = pd.DataFrame({
+    "subsystem_id": ["temperature", "kinematic", "configuration"],
+    "machine_head_id": ["MH1", "MH1", "MH1"],
+    "description": [
+        "Temperature monitoring system",
+        "Motion and spatial system",
+        "Operational configuration settings"
+    ]
+})
+
+
+machine_head_temperature_data = pd.DataFrame({
+    "parameter_name": [
+        "Head Temperature (RTD 1)(°F)",
+        "Head Temp: Warning High Level",
+        "Head Temp: Warning Low Level"
+    ],
+    "subsystem_id": "temperature"
+})
+
+print(machine_head_temperature_data)
+
+machine_head_configuration_data = pd.DataFrame({
+    "parameter_name": [
+        "Motion Compensation Active"
+    ],
+    
+})
+
+
+machine_head_kinematic_types = pd.DataFrame({
+    "kinematic_id": ["position", "velocity"],
+    "subsystem_id": ["kinematic", "kinematic"],
+    "description": [
+        "Spatial position data",
+        "Velocity data"
+    ]
+})
+
+print(machine_head_kinematic_types)
+
 #tables to be stored under kinematic data
-position_data = pd.DataFrame({
-    "parameter_name": ["Pos X(inch)", "Pos Y(inch)", "Pos Z(inch)"],
+machine_head_position_data = pd.DataFrame({
+    "parameter_name": ["Pos X (inch)", "Pos Y (inch)", "Pos Z (inch)"],
     "description": [
         "X position of the laser head in inches",
         "Y position of the laser head in inches",
         "Z position of the laser head in inches"
-    ]
+    ],
+    "kinematic_id": "position"
 })
-position_data["kinematic_id"] = "position"
 
 velocity_data = pd.DataFrame({
-    "parameter_name": ["Velocity X", "Velocity Y", "Velocity Z"],    
+    "parameter_name": ["Velocity X", "Velocity Y", "Velocity Z"],
     "description": [
         "X velocity of the laser head in inches per second",
         "Y velocity of the laser head in inches per second",
         "Z velocity of the laser head in inches per second"
-    ]
+    ],
+    "kinematic_id": "velocity"
 })
-velocity_data["kinematic_id"] = "velocity"  
 
 
 '''
@@ -41,59 +106,119 @@ The velocity data includes the X, Y, and Z velocities of the laser head in inche
 This information is crucial for understanding the motion of the laser during the additive manufacturing process 
 and can be used to analyze the relationship between the laser's movement and the resulting melt pool characteristics.
 '''
-
 #Spatiotemporal data
+timestamp_data = pd.DataFrame({
+    "parameter_name": ["Layer #", "Timestamp"],
+    "description": [
+        "Current layer number",
+        "Timestamp of the data point, converted to UTC"
+    ],
+    "subsystem_id": "kinematic"
+})
 
-timestamp_data = pd.DataFrame({"Layer #": "Current layer number", "TimeStamp": "Timestamp of the data point, converted to UTC"})
-spatiotemporal_data = pd.DataFrame({position_data, timestamp_data})
+
+#Create subtables
+spatiotemporal_data = pd.concat(
+    [timestamp_data, machine_head_position_data, velocity_data],
+    ignore_index=True
+)
+
+print(spatiotemporal_data)
+
+#overlapping x data with kinematic data though
 
 #%%
 #--Optics---
-'''optics_units'''
 
-#Laser data
-laser_data = pd.DataFrame({
-    "parameter_name": [
-        "Laser On",
-        "Pos Alps(inch)",
-        "Alps Spot Size(inch)",
-        "Optics Box Pressure Sensor",
-        "Laser Setpoint",
-        "Laser Power (from laser)",
-        "Laser Water Flow (L/min)",
-        "Laser Water Temp (Â°C)",
-        "Laser On Time (ms)",
-        "Feed Fiber FFBD (mV)",
-        "Process Fiber FFBD (mV)",
-        "Power From Meter",
-        "Beam Size From Meter (inch)",
-        "Beam Pos X From Meter (inch)",
-        "Beam Pos Y From Meter (inch)",
-        "Power From Meter (uncompensated)",
-        "Beam Flags From Meter"
+
+optics_unit = pd.DataFrame({
+    "optics_unit_id": ["OPT1"],
+    "description": ["Laser optics assembly"]
+})
+
+optics_subsystems = pd.DataFrame({
+    "subsystem_id": [
+        "laser_core",
+        "cooling_system",
+        "beam_monitor",
+        "fiber_diagnostics",
+        "optics_environment",
+        "alps_positioning"
     ],
+    "optics_unit_id": ["OPT1"] * 6,
     "description": [
-        "Laser On/Off status",
-        "Laser position in Alps coordinate system",
-        "Laser spot size in inches",
-        "Pressure sensor in optics box",
-        "Laser power set by the user",
-        "Laser power measured from the laser source",
-        "Laser cooling water flow rate",
-        "Laser cooling water temperature",
-        "Total laser firing time",
-        "Feed fiber back reflection voltage",
-        "Process fiber back reflection voltage",
-        "Measured laser power from meter",
-        "Measured beam size",
-        "Measured beam X position",
-        "Measured beam Y position",
-        "Uncompensated power reading",
-        "Beam status flags"
+        "Laser emission and control",
+        "Laser cooling and water system",
+        "Beam measurement and metrology",
+        "Fiber back reflection diagnostics",
+        "Optics enclosure environmental monitoring",
+        "ALPS beam positioning system"
     ]
 })
 
-print(laser_data)
+laser_core_data = pd.DataFrame({
+    "parameter_name": [
+        "Laser On",
+        "Laser Setpoint",
+        "Laser Power (from laser)",
+        "Laser On Time (ms)"
+    ],
+    "subsystem_id": "laser_core"
+})
+
+cooling_data = pd.DataFrame({
+    "parameter_name": [
+        "Laser Water Flow (L/min)",
+        "Laser Water Temp (°C)"
+    ],
+    "subsystem_id": "cooling_system"
+})
+
+beam_monitor_data = pd.DataFrame({
+    "parameter_name": [
+        "Power From Meter",
+        "Power From Meter (uncompensated)",
+        "Beam Size From Meter (inch)",
+        "Beam Pos X From Meter (inch)",
+        "Beam Pos Y From Meter (inch)",
+        "Beam Flags From Meter"
+    ],
+    "subsystem_id": "beam_monitor"
+})
+
+fiber_diagnostics_data = pd.DataFrame({
+    "parameter_name": [
+        "Feed Fiber FFBD (mV)",
+        "Process Fiber FFBD (mV)"
+    ],
+    "subsystem_id": "fiber_diagnostics"
+})
+
+optics_environment_data = pd.DataFrame({
+    "parameter_name": [
+        "Optics Box Pressure Sensor"
+    ],
+    "subsystem_id": "optics_environment"
+})
+
+alps_data = pd.DataFrame({
+    "parameter_name": [
+        "Pos Alps (inch)",
+        "Alps Spot Size (inch)"
+    ],
+    "subsystem_id": "alps_positioning"
+})
+
+
+all_optics_parameters = pd.concat([
+    laser_core_data,
+    cooling_data,
+    beam_monitor_data,
+    fiber_diagnostics_data,
+    optics_environment_data,
+    alps_data
+], ignore_index=True)
+
 
 
 #%%
@@ -139,7 +264,6 @@ so center_purge_data["unit_id"] will access column unit_id inside center_purge_d
 but this column does not exist yet, so pandas will create it 
 
 '''
-
 
 #Single Child table
 hopper_data = pd.DataFrame({
@@ -245,25 +369,6 @@ argon_data = pd.concat([hopper_data[hopper_data["parameter_name"].str.contains("
                         center_purge_data], ignore_index=True)
 
 
-#%%
-
-machine_head_data = pd.DataFrame({
-    "parameter_name": [
-        "Motion Compensation Active",
-        "Head Temperature (RTD 1)(Â°F)",
-        "Head Temp: Warning High Level",
-        "Head Temp: Warning Low Level"
-    ]
-})
-RPMI_machine_data = pd.DataFrame({
-    "parameter_name": [
-        "Box Pressure",
-        "Dust Collector Diff Pressure",
-        "Powder Manifold Pressure Sensor",
-        "Path Setpoint Velocity(inch/min)",
-        "Toolcode Execution Time"
-    ]
-})
 
 #%%
 #external sensor data
